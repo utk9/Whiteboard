a5e5d40048153d6630abc18a220b53a716f656db
utk9 <utkarshlamba.2@gmail.com>
2016-07-31 22:19:44 -0400

    Adds error page and improves home UI

--- routes/whiteBoard.js
+++ routes/whiteBoard.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('whiteBoard', { title: 'Express' });
 });
 
+router.get('/test', function(req, res, next) {
+	res.send("test route");
+});
+
 module.exports = router;
