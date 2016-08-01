75c983c96c174b4454e89a4aae3864c8955578bf
utk9 <utkarshlamba.2@gmail.com>
2016-08-01 16:36:15 -0400

    Adds api to create new whiteboard

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ app.use(function(req, res, next) {
 // will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
+  	console.log(err);
     res.status(err.status || 500);
     res.render('error');
   });
@@ -44,6 +45,7 @@ if (app.get('env') === 'development') {
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
+   console.log(err);
   res.status(err.status || 500);
   res.render('error');
 });
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+var canvasMap = new Object();
+
+var Canvas = function (name) {
+	this.name = name;
+}
+
+//Routes ======================================================
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
 
 router.get('/create', function(req, res) {
@@ -14,10 +20,47 @@ router.get('/join', function(req, res) {
 	res.render('join');
 });
 
-router.get('/:id', function(req, res) {
-	var id = req.param.id;
-	console.log(id);
-	res.render('whiteBoard');
-})
+router.post('/api/canvas', function (req, res) {
+	var name = req.body.name;
+	if (canvasMap.hasOwnProperty(name)) {
+		res.status(403).json({
+			success: false,
+			reason: "This name already exists."
+		});
+	} else {
+		canvasMap[name] = new Canvas(name);
+		res.json({
+			success: true,
+			name: name
+		});
+	}
+});
+
+//API =========================================================
+router.get('/api/canvas', function(req, res) {
+	console.log(JSON.stringify(canvasMap));
+	res.send(canvasMap);
+});
+
+router.get('/api/canvas/:name', function(req, res) {
+	res.json(canvasMap[req.params.name]);
+});
+
+//IO =========================================================
+
+
+//whiteboard route
+router.get('/:name', function(req, res) {
+	var name = req.params.name;
+	if (canvasMap.hasOwnProperty(name)) {
+		res.render('whiteBoard');
+	} else {
+		res.status(404).render('error');
+	}
+
+	
+});
+
+
 
 module.exports = router;
