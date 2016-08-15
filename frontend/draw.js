cd6c1b99cfd348e6098c785c18380077e93ceefc
drop-melissa <melissa@earnwithdrop.com>
2016-08-15 19:36:59 -0400

    Modifies code to fit module structure

--- frontend/draw.js
+++ frontend/draw.js
@@ -1,6 +1,59 @@
-var tools = require('./tools.js');
-var ToolAttributes = require('./ToolAttributes.js');
+var canvas = require('./domNodes').canvas;
+var toolAttributes = require('./toolAttributes').attributes
+var marker = toolAttributes.marker
 
-tools();
+var drawing = false;
+var prevPos = { x: 0, y: 0 }
+var curPos = { x: 0, y: 0 }
+var ctx = canvas.getContext('2d');
 
-console.log(ToolAttributes.getName());
+var draw = function(type, e) {
+
+  if (type === 'down') {
+    setCurrentPos(e);
+
+    // Draw the first dot
+    ctx.beginPath();
+    ctx.fillStyle = marker.color;
+    ctx.arc(curPos.x, curPos.y, marker.size/2, 0, 2 * Math.PI);
+    ctx.fill();
+    ctx.closePath();
+
+    drawing = true;
+
+  } else if (type === 'move') {
+    if (drawing) {
+      setPrevPos();
+      setCurrentPos(e);
+      stroke();
+    }
+  } else if (type === 'up' || type === 'out') {
+    drawing = false;
+  }
+
+}
+
+function setPrevPos() {
+  prevPos.x = curPos.x;
+  prevPos.y = curPos.y;
+}
+
+function setCurrentPos(e) {
+  curPos.x = e.clientX - canvas.offsetLeft;
+  curPos.y = e.clientY - canvas.offsetTop;
+}
+
+function stroke() {
+  ctx.beginPath();
+
+  ctx.lineWidth = marker.size;
+  ctx.strokeStyle = marker.color;
+
+  ctx.lineJoin = ctx.lineCap = 'round';
+  ctx.moveTo(prevPos.x, prevPos.y);
+  ctx.lineTo(curPos.x, curPos.y);
+  ctx.stroke();
+  ctx.closePath();
+}
+
+module.exports = draw
